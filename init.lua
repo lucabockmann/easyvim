f3c09a3214aa8936fce0437082a215677e5b3cee
Luca Bockmann <hey@studiobylucabockmann.de>
2025-02-24 10:35:19 +0100

    fix small things

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("onedarkpro").setup({
+  options = {
+    cursorline = true, -- Use cursorline highlighting?
+    transparency = true, -- Use a transparent background?
+    terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
+    lualine_transparency = true, -- Center bar transparency?
+    highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
+  },
+})
+
+vim.cmd("colorscheme tokyonight")
